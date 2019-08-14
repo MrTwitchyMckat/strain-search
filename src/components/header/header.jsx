bae434b708485a50286043d2d79e2b419083a595
MrTwitchyMckat <vivekbhardwajnv@gmail.com>
2019-08-13 22:35:29 -0700

    Skeleton of app and began making header

---
+++ src/components/header/header.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class header extends Component {
+    state = { 
+        small: false
+     }
+    render() { 
+        return ( 
+            <div className="mh">
+            </div>
+         );
+    }
+}
+ 
+export default header;
