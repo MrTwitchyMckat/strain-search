81dcc6677fc390f7cbeee141a7c14322ca0b675b
MrTwitchyMckat <vivekbhardwajnv@gmail.com>
2019-08-14 21:44:02 -0700

    Removed comments from index & added header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import './App.css';
+import Header from './components/header/header';
 
 function App() {
   return (
     <div className="App">
+      <Header />
     </div>
   );
 }
